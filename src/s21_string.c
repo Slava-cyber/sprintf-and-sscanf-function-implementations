282640db46002151945c118b4f3b08261296576c
Lytle Ashlyn <lashlyn@se-h6.nsk.21-school.ru>
2021-10-30 19:05:20 +0700

    add header,body files

---
+++ src/s21_string.c
@@ -0,0 +1,84 @@
+#include "../groupProject/s21_string.h"
+
+
+void * s21_memcpy(void *dest, const void *src, s21_size_t n) {
+    
+    char *p = (char*)dest;
+    char *p1 = (char*)src;
+    while (n--) {
+        *p = *p1;
+        p++;
+        p1++;
+    }
+    
+    return dest;
+}
+
+
+s21_size_t s21_strlen(const char *str) {
+    s21_size_t len = 0;
+    for(; str[len]; len++);
+    return len;
+}
+
+
+char * s_21Strchr(const char * str,int c) {
+    
+    char const * buf = NULL;
+    while (*str) {
+        if (*str == c) {
+            break;
+        }
+        str++;
+    }
+    
+    if (*str == c) {
+        buf = str;
+    }
+    return (char *) buf;
+    
+}
+
+
+
+s21_size_t s_21Strspn(const char* str1, const char* str2) {
+    s21_size_t n;
+    const char* p;
+    for(n = 0; *str1; str1++, n++) {
+        for(p = str2; *p && *p != *str1; p++);
+        if (!*p)
+            break;
+    }
+    return n;
+}
+
+
+int s_21strcspn(const char * str1,const char * str2) {
+    
+    s21_size_t lentght1 = s21_strlen(str1);
+    s21_size_t lentght2 = s21_strlen(str2);
+    
+    int supp;
+    int i;
+    int count;
+    
+    for( i = 0;i < lentght1;i++) {
+        if(supp) {
+            break;
+        }
+        for(int j =0;j < lentght2;j++) {
+            if(str1[i] == str2[j]) {
+                count = i;
+                supp ++;
+                break;
+            }
+        }
+    }
+    if(!supp) {
+        count = i;
+    }
+    
+    
+    
+    return count;
+}
---
+++ src/s21_string.h
@@ -0,0 +1,15 @@
+#ifndef S21_STRING_H_
+#define S21_STRING_H_
+
+#define NULL ((char *)0)
+
+typedef unsigned s21_size_t;
+
+
+void * s21_memcpy(void *dest, const void *src, s21_size_t n);
+s21_size_t s_21Strspn(const char* str1, const char* str2);
+char * s_21Strchr(const char * str,int ch);
+int s_21strcspn(const char * str1,const char * str2);
+s21_size_t s21_strlen(const char *str);
+
+#endif
